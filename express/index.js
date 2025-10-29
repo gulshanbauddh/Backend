8c6540a8b7ae457725d2a4913edffd08e43bcab2
gulshanbauddh <gulshanbncet@gmail.com>
2025-10-29 23:56:07 +0530

    learn 29/10/25

--- express/index.js
+++ express/index.js
@@ -1,10 +1,27 @@
-let express=require("express")
-let app=express()
+let express = require("express");
+let app = express();
+app.use(express.json());
 
-app.get("/",(req, res)=>{
-  res.send({status:1, msg:"Home Page API" })
-})
-app.get("/news",(req, res)=>{
-  res.send({status:1, msg:"NEWS Page API" })
-})
-app.listen("8000")
+// get Methed used for display data
+app.get("/", (req, res) => {
+  res.send({ status: 1, msg: "Home Page API" });
+});
+app.get("/news", (req, res) => {
+  res.send({ status: 1, msg: "NEWS Page API" });
+});
+app.get("/about", (req, res) => {
+  res.send({ status: 1, msg: "About Page API" });
+});
+
+// Post method is used for securly. It could not display.
+app.post("/login", (req, res) => {
+  console.log(req.body);
+  res.send({
+    status: 1,
+    msg: "Login API",
+    dataData: req.body,
+    queryData:req.query
+  });
+});
+
+app.listen("8000");
